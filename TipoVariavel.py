cae30d078e5f3ad707257401ea0fb7af020baf09
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-08-12 23:16:29 -0300

    Update and rename modulo3.py to TipoVariavel.py

---
+++ TipoVariavel.py
@@ -0,0 +1,13 @@
+codigo = 10
+salario = 1500.00
+nome = 'Jose'
+situação = True
+
+tipo = type (salario)
+
+print(salario)
+print(tipo)
+
+print("Código: ", codigo, 'Nome: ', nome, "O salario atual é de ", salario)
+
+print("Codigo: "+ str (codigo)+ " Nome: "+ nome+ " O salario atual é de: "+str(salario))
--- modulo3.py
+++
@@ -1,9 +0,0 @@
-codigo = 10
-salario = 1500.00
-nome = 'Jose'
-situação = True
-
-tipo = type (salario)
-
-print(salario)
-print(tipo)
